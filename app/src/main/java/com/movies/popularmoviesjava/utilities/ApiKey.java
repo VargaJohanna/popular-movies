41f325ae97f210fa01122adced66e3bf26818fc2
VargaJohanna <varga.johanna@gmail.com>
2019-02-09 18:49:33 +0000

    Take the ApiKey class out of gitignore and override the api key

---
+++ app/src/main/java/com/movies/popularmoviesjava/utilities/ApiKey.java
@@ -0,0 +1,7 @@
+package com.movies.popularmoviesjava.utilities;
+
+public class ApiKey {
+    // According to the implementation guide the api key should not be shared.
+    // Please update the key below to be able to build the app.
+    public static final String KEY = "fake_api_key";
+}
